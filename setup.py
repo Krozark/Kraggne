5981d996b2b75f892049a82c4f320b8d8108fa17
krozark <maxime.barbier1991@gmail.com>
2012-06-09 16:20:07 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Kraggen',
+    version='0.1',
+    description='A django cms project',
+    long_description=open('README').read(),
+    author='Maxime Barbier',
+    author_email='maxime.barbier1991@gmail.com',
+    url='https://github.com/Krozark/Kraggne/tree/master',
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 0 ',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+    install_requires = [
+        "django-generic-flatblocks",
+        "django-frontadmin",
+    ],
+    zip_safe=False,
+)
