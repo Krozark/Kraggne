f8a2c6de5481506747e02a1a0c8ea6bd16ca910d
Krozark <maxime.barbier1991@gmail.com>
2014-01-14 12:42:03 +0100

    add custom multiselect

--- Kraggne/parser.py
+++ Kraggne/parser.py
@@ -15,41 +15,31 @@ def get_model_from_include(link,return_app_model=False):
         return get_model(app,model),app,model
     return get_model(app,model)
 
-
-def get_model_and_url_from_detail(link,return_app_model=False):
-    url = link[len('detail('):]
+def get_model_and_url_from(prefix,link,return_app_model=False):
+    url = link[len(prefix+"("):]
     i = url.rfind(',')
     if i <=0:
-        raise ValidationError(_('imposible to find "," syntaxe: detail(url,app.model)'))
+        raise ValidationError(_('imposible to find "," syntaxe: %s(url,app.model)' % prefix))
      
     app = url[i+1:].replace(')','')
     url = url[:i].replace('"','').replace("'",'')
     app = app.split('.')
 
     if len(app) != 2:
-        raise ValidationError(_('imposible to find one "." in %s syntaxe: detail(url,app.model)' % '.'.join(app)))
+        raise ValidationError(_('imposible to find one "." in %s syntaxe: %s(url,app.model)' % ('.'.join(app),prefix)))
 
     app,model = app[0],app[1]
 
     if return_app_model:
         return get_model(app,model),url,app,model
     return get_model(app,model),url
+
+
+def get_model_and_url_from_detail(link,return_app_model=False):
+    return get_model_and_url_from("detail",link,return_app_model)
 
 def get_model_and_url_from_list(link,return_app_model=False):
-    url = link[len('list('):]
-    i = url.rfind(',')
-    if i <=0:
-        raise ValidationError(_('imposible to find "," syntaxe: list(url,app.model)'))
+    return get_model_and_url_from("list",link,return_app_model)
 
-    app = url[i+1:].replace(')','')
-    url = url[:i].replace('"','').replace("'",'')
-    app = app.split('.')
-
-    if len(app) != 2:
-        raise ValidationError(_('imposible to find one "." in %s syntaxe: list(url,app.model)' % '.'.join(app)))
-
-    app,model = app[0],app[1]
-
-    if return_app_model:
-        return get_model(app,model),url,app,model
-    return get_model(app,model),url
+def get_model_and_url_from_form(link,return_app_model=False):
+    return get_model_and_url_from("form",link,return_app_model)
